8535685e8d5740ec1c4446dded2852d44333d60a
Yester Selga <yesterselga1493@gmail.com>
2017-08-29 12:19:23 +0800

    Updated sample app

--- app/src/main/java/com/ybs/countrypickerexample/MainActivity.java
+++ app/src/main/java/com/ybs/countrypickerexample/MainActivity.java
@@ -1,13 +1,48 @@
 package com.ybs.countrypickerexample;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.ybs.countrypicker.CountryPicker;
+import com.ybs.countrypicker.CountryPickerListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    CountryPicker picker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        picker = CountryPicker.newInstance("Select Country");  // dialog title
+        picker.setListener(new CountryPickerListener() {
+            @Override
+            public void onSelectCountry(String name, String code, String dialCode, int flagDrawableResID) {
+
+                EditText countryCode = (EditText)findViewById(R.id.countryCode);
+                EditText countryName = (EditText)findViewById(R.id.countryName);
+                EditText countryDialCode = (EditText)findViewById(R.id.countryDialCode);
+                ImageView countryIcon = (ImageView)findViewById(R.id.countryIcon);
+
+                countryCode.setText(code);
+                countryName.setText(name);
+                countryDialCode.setText(dialCode);
+                countryIcon.setImageResource(flagDrawableResID);
+
+            }
+        });
+
+
+
     }
+
+    public void openPicker(View view){
+        picker.show(getSupportFragmentManager(), "COUNTRY_PICKER");
+    }
+
 }
