5612d8cd50fbeaee50331de3c136199c2fb6987d
Yester Selga <yesterselga1493@gmail.com>
2017-08-29 10:49:12 +0800

    First commit

---
+++ countrypicker/src/main/java/com/ybs/countrypicker/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.ybs.countrypicker;
+
+import android.util.Log;
+
+/**
+ * Created by mispc1 on 8/29/17.
+ */
+
+public class HelloWorld {
+
+    public static void sayHello(){
+        Log.d("HELLO", "Hello World");
+    }
+
+}
